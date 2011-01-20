69cddf6ecb0a8036ed4418c50bd6bd7fdc0b0c60
Jin Liu <jinliu03@gmail.com>
2011-01-20 14:26:18 -0500

    Delte main controller

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,4 @@ FirstApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   
-  match "/main/" => 'main#index'
 end
